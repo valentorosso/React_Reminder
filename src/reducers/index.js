7996b111d1be2c1f43be0ab3d180fcdad5222721
Yan Kozel <yanik.kozel@gmail.com>
2020-02-16 00:12:28 +0300

    Add Reminder React App

---
+++ src/reducers/index.js
@@ -0,0 +1,39 @@
+import {ADD_REMINDER, DELETE_REMINDER, CLEAR_REMINDER} from '../constants';
+
+
+const reminder = (action) => {
+    let {text, dueDate} = action;
+    return {
+        id: Math.random(),
+        text,
+        dueDate
+    }
+}
+
+const removeById = (state = [], id) => {
+    const reminders = state.filter(reminder => reminder.id !== id);
+    return reminders;
+}
+
+const reminders = (state = [], action) => {
+    let reminders = null;
+
+    switch (action.type) {
+        case ADD_REMINDER:
+            reminders = [...state, reminder(action)];
+
+            return reminders;
+        case DELETE_REMINDER:
+            reminders = removeById(state, action.id);
+
+            return reminders;
+        case CLEAR_REMINDER:
+            reminders = [];
+
+            return reminders;
+        default:
+            return state;
+    }
+}
+
+export default reminders;
